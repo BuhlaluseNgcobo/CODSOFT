8e4bf9ad16a933bc15dc5a59d58250b64e276813
BuhlaluseNgcobo <162983241+BuhlaluseNgcobo@users.noreply.github.com>
2024-03-17 19:09:22 +0200

    Add files via upload

---
+++ ATM3.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atm3;
+
+/**
+ *
+ * @author gugu
+ */
+import java.util.Scanner;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class ATM3 {
+    private BankAccount account;
+
+    public ATM3(BankAccount account) {
+        this.account = account;
+    }
+
+    public void displayMenu() {
+        System.out.println("Welcome to the ATM!");
+        System.out.println("1. Withdraw");
+        System.out.println("2. Deposit");
+        System.out.println("3. Check Balance");
+        System.out.println("4. Exit");
+    }
+
+    public void processOption(int option) {
+        Scanner scanner = new Scanner(System.in);
+
+        switch (option) {
+            case 1:
+                System.out.print("Enter amount to withdraw: ");
+                double withdrawAmount = scanner.nextDouble();
+                if (account.withdraw(withdrawAmount)) {
+                    System.out.println("Withdrawal successful.");
+                } else {
+                    System.out.println("Insufficient balance.");
+                }
+                break;
+            case 2:
+                System.out.print("Enter amount to deposit: ");
+                double depositAmount = scanner.nextDouble();
+                account.deposit(depositAmount);
+                System.out.println("Deposit successful.");
+                break;
+            case 3:
+                System.out.println("Your balance is: " + account.getBalance());
+                break;
+            case 4:
+                System.out.println("Exiting ATM. Thank you!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
+        }
+    }
+
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000); // Initial balance of 1000
+        ATM3 atm = new ATM3(userAccount);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            atm.displayMenu();
+            System.out.print("Select an option: ");
+            int option = scanner.nextInt();
+            atm.processOption(option);
+        }
+    }
+}
